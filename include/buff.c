43ebfcbfec6b210e0b1ead74f0bd8f60524cb5e7
zhurunshen <runshen.zhu@gmail.com>
2014-10-14 23:46:22 +0800

    redefine data structure

--- include/buff.c
+++ include/buff.c
@@ -8,25 +8,31 @@
 
 void initContactorTable()
 {
-	memset(contactorTable, 0, sizeof(struct in_addr) * hashTableSize);
+	int i;
+	for(i = 0; i < hashTableSize; i++){
+		pthread_mutex_init( &contactorTable[i].lock, NULL );
+		contactorTable[i].addr = NULL;
+	}
 }
-
 
 int contactorReg(char *name, const char *ip)
 {
 	//get hash value
 	unsigned int val = hash_pjw(name);
 
+	pthread_mutex_lock( &contactorTable[val].lock );
 	//register
-	if( contactorTable[val] == NULL ){//check succ
+	if( contactorTable[val].addr == NULL ){//check succ
 		//contactorTable[val] = *(uint32_t *)ip;
 		struct in_addr *tmp = (struct in_addr*) malloc(sizeof(struct in_addr));
 		assert(tmp != NULL); 
 		inet_pton_wrapper(AF_INET, ip, tmp);
-		contactorTable[val] = tmp;
+		contactorTable[val].addr = tmp;
+		pthread_mutex_unlock( &contactorTable[val].lock );
 		return 1;
 	}
 	else{	//check fail
+		pthread_mutex_unlock( &contactorTable[val].lock );
 		return 0;
 	}
 
@@ -35,25 +41,35 @@ int contactorReg(char *name, const char *ip)
 int contactorIsExist(char *name)
 {
 	unsigned int val = hash_pjw(name);
-	return contactorTable[val] == NULL? 0:1;
+	return contactorTable[val].addr == NULL? 0:1;
 }
 
 struct in_addr contactorGetIp(char *name)
 {
 	unsigned int val = hash_pjw(name);
-	return *contactorTable[val];
+	return *(contactorTable[val].addr);
 }
 
 void contactorOffline(char *name)
 {
 	unsigned int val = hash_pjw(name);
-	assert( contactorTable[val] != NULL );
-	free(contactorTable[val]);
+	assert( contactorTable[val].addr != NULL );
 
-	contactorTable[val] = NULL;
+	pthread_mutex_lock( &contactorTable[val].lock );
+	free(contactorTable[val].addr);
+	contactorTable[val].addr = NULL;
+	pthread_mutex_unlock( &contactorTable[val].lock );
 }
 
-int main()
+void contactorTableDestory()
+{
+	int i;
+	for(i = 0; i < hashTableSize; i++){
+		pthread_mutex_destory( &contactorTable[i].lock );	
+	}
+}
+
+void buffTest()
 {
 
 	initContactorTable();
@@ -71,7 +87,17 @@ int main()
 	//assert( (testBuff != NULL) );
 
 	inet_ntop(AF_INET, (void *)&testBuff, IP, 16);
-	printf("inet_ntop: %s\n", IP);
+	//printf("inet_ntop: %s\n", IP);
+	assert( (strcmp(IP, ip) == 0) );
+	contactorTableDestory();
+	//return 0;
+}
 
+
+/*
+int main()
+{
+	buffTest();
 	return 0;
 }
+*/
--- include/buff.h
+++ include/buff.h
@@ -1,19 +1,45 @@
 #ifndef BUFF_Header_h
 #define BUFF_Header_h
 
+#include <pthread.h>
+#include <stdint.h>
 #include "hash.h"
 
-//#define NULL (void *)0
-
-/*
-typedef struct buffNode
+/********ContactorTable*********/
+//struct in_addr *contactorTable[hashTableSize];
+struct _contactorNode
 {
-
+	pthread_mutex_t lock;
+	struct in_addr *addr;
+	//struct _contactorNode(): lock(PTHREAD_MUTEX_INITIALIZER), addr(NULL) {};
+	//struct contactorNode() : lock(PTHREAD_MUTEX_INITIALIZER), addr(NULL) {};
 };
-*/
-//uint32_t contactorTable[hashTableSize] = {0};
-struct in_addr *contactorTable[hashTableSize];
-//void initContactorTable();
+typedef struct _contactorNode contactorNode;
+contactorNode contactorTable[hashTableSize];
+
+void initContactorTable();
+int contactorReg(char *name, const char *ip);
+int contactorIsExist(char *name);
+struct in_addr contactorGetIp(char *name);
+void contactorOffline(char *name);
+void contactorTableDestory();
+
+//test
+void buffTest();
+
+/*************MSG**************/
+
+#define MSGREG 		0x00	//注册
+#define MSGOFFLINE	0x01	//下线
+#define MSGCHAT		0x02	//交流
+typedef struct 
+{
+	uint8_t type;
+	char src[32];
+	char des[32];
+	char data[512];
+} MSGBuff;
+
 
 
 
